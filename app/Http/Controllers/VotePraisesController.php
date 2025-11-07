500bd44fcacb76a941ade27942821601dc1cab79
Mateus Brandão <mateusfleria@gmail.com>
2025-11-06 23:18:21 -0300

    fix: Correção de sintaxe

--- app/Http/Controllers/VotePraisesController.php
+++ app/Http/Controllers/VotePraisesController.php
@@ -21,7 +21,7 @@ class VotePraisesController extends Controller{
     if($status === 'Em Votação' || $status === 'Finalizado') $praises = $this->praisesInVoting();
     if($status === 'Finalizado'){
       $selecteds = [
-        0 => ['i3E_V9Ik85I', 'Y6-zkoPqTPA']
+        0 => ['i3E_V9Ik85I', 'Y6-zkoPqTPA'],
         1 => ['7GWZwO0MdsY', 'ozJIN6Pej8w']
       ][VotePraisesController::getIndexVotation()];
       $praises = array_filter($praises, function ($praise) use ($selecteds) {
